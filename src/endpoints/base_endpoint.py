0820e831f05ffd8592e4e328488906cf1578743f
Elliott Stam <elliott.stam@gmail.com>
2020-05-05 16:30:51 -0700

    added support for optional url parameters on endpoints

--- src/endpoints/base_endpoint.py
+++ src/endpoints/base_endpoint.py
@@ -2,6 +2,26 @@ class BaseEndpoint:
     def __init__(self, version, sandbox=False):
         self._version = version
         self._sandbox = sandbox
+        self._parameter_dict = {}
+
+    def _params_exist(self):
+        if self.parameter_dict:
+            return list(self.parameter_dict.keys())
+        else:
+            return []
+
+    def _params_text(self):
+        if self.parameter_dict:
+            return list(self.parameter_dict.values())
+        else:
+            return []
+
+    def _append_url_parameters(self, url):
+        text_to_append = "?" if any(self._params_exist()) else ""
+        for i, text in enumerate(self._params_text()):
+            if self._params_exist()[i]:
+                text_to_append += text if text_to_append.endswith('?') else ('&' + text)
+        return "{0}{1}".format(url, text_to_append)
 
     @property
     def version(self):
@@ -10,6 +30,10 @@ class BaseEndpoint:
     @property
     def sandbox(self):
         return self._sandbox
+
+    @property
+    def parameter_dict(self):
+        return self._parameter_dict
 
     @property
     def base_url(self):
