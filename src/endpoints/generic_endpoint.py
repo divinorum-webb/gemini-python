0bc71839647d7181afeb3024a7d25a753cb89cc9
Elliott Stam <elliott.stam@gmail.com>
2020-05-06 01:08:58 -0700

    replaced generic endpoint classes with GenericEndpoint

--- src/endpoints/generic_endpoint.py
+++ src/endpoints/generic_endpoint.py
@@ -2,14 +2,16 @@ from .base_endpoint import BaseEndpoint
 from src.decorators import verify_api_method_exists
 
 
-class PriceFeedEndpoint(BaseEndpoint):
-    def __init__(self, version='v1', sandbox=False):
+class GenericEndpoint(BaseEndpoint):
+    def __init__(self, endpoint_text, version='v1', sandbox=False):
         """
-        Initializes the PriceFeedEndpoint class.
+        Initializes the GenericEndpoint class.
+        :param str endpoint_text: the tail end of the endpoint url to append to the base url
         :param str version: the api version to use
         :param bool sandbox: True if using the sandbox api, False otherwise
         """
         super().__init__(version, sandbox)
+        self._endpoint_text = endpoint_text
 
     @verify_api_method_exists('v1')
     def get_endpoint(self):
@@ -17,4 +19,4 @@ class PriceFeedEndpoint(BaseEndpoint):
             url = self.base_sandbox_url
         else:
             url = self.base_url
-        return "{0}/{1}/pricefeed".format(url, self.version)
+        return "{0}/{1}/{2}".format(url, self.version, self._endpoint_text)
